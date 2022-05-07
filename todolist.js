2c18aa1fa5522d861f8e5208556090d2420ef45d
TomAk1993 <96138157+TomAk1993@users.noreply.github.com>
2022-05-07 10:35:33 +0100

    Created a remove button at the top of the list

--- todolist.js
+++ todolist.js
@@ -1,10 +1,18 @@
 const formInput = document.getElementById('new-task-input');
 const addTask = document.getElementById('new-task-submit');
-const taskText = document.getElementById('task-text')
+const taskText = document.getElementById('text')
+const clear = document.getElementById('clear')
 
 addTask.addEventListener('click', function(e){
     e.preventDefault();
-    const p = document.createElement('p')
-    p.innerHTML = formInput.value;
-    taskText.appendChild(p);
+    const li = document.createElement('li')
+    li.innerHTML = formInput.value;
+    taskText.appendChild(li);
 })
+
+clear.addEventListener('click', function(e){
+    e.preventDefault();
+    taskText.remove();
+})
+
+
